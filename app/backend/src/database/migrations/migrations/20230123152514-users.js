03defa1577c2b1b1a90b6331b181636c957d1ddd
Samuel Hyrum <samuelhymp@gmail.com>
2023-01-23 15:21:34 -0300

    Erros na criação da tabela

---
+++ app/backend/src/database/migrations/migrations/20230123152514-users.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
