1c9d5f86e6af8e6f37d4fa2c0924a335c9aa58c5
Samuel Hyrum <samuelhymp@gmail.com>
2023-01-25 16:29:51 -0300

    ROTAS GET/MATCHES USANDO QUERY STRING TRUE OR FALSE PARA MATCHES IN PROGRESS

--- app/backend/src/database/controllers/matches.controllers.ts
+++ app/backend/src/database/controllers/matches.controllers.ts
@@ -4,9 +4,25 @@ import MatchService from '../services/matchService';
 export default class MatchController {
   constructor(private matchService = new MatchService()) {}
 
-  public getAllTeams = async (_req: Request, res: Response) => {
+  public getAllTeams = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
     const returnAllMatches = await this.matchService.getAllMatches();
 
-    return res.status(200).json(returnAllMatches);
+    if (!inProgress) {
+      return res.status(200).json(returnAllMatches);
+    }
+
+    if (inProgress === 'true') {
+      const matchesLives = returnAllMatches
+        .filter((match) => match.dataValues.inProgress === true);
+      return res.status(200).json(matchesLives);
+    }
+
+    if (inProgress === 'false') {
+      const matchesEnd = returnAllMatches
+        .filter((match) => match.dataValues.inProgress === false);
+      return res.status(200).json(matchesEnd);
+    }
   };
 }
